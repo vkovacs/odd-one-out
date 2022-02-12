27c3fd8a1cdf0732e77115d4c7bcde4c97fe0e44
Viktor Kovacs <crusader8@gmail.com>
2022-02-12 11:39:12 +0100

    extract method

--- src/main/groovy/hu/crs/exerciser/Application.groovy
+++ src/main/groovy/hu/crs/exerciser/Application.groovy
@@ -1,29 +1,34 @@
 package hu.crs.exerciser
 
+import groovy.transform.ToString
+
 class Application {
 
     public static final int MAX_VALUE = 10
+    private static final List<Character> operators = ['+', '-'] as List<Character>
+    private static final Random random = new Random()
 
     static void main(String[] args) {
-        List<Character> operators = ['+', '-'] as List<Character>
-        Set<Integer> results = []
-        Random random = new Random()
 
+        5.times {
+            println exercise()
+        }
+    }
+
+    private static Exercise exercise() {
+        Set<String> expressions = []
+
+        Set<Integer> results = []
         while (results.size() < 10) {
             def operand0 = random.nextInt(MAX_VALUE * 2)
             def operand1 = random.nextInt(MAX_VALUE)
             def operator = operators[random.nextInt(operators.size())]
+            def expression = "${operand0} ${operator} ${operand1}".toString()
+            expressions << expression
 
-            def operand0Padded = operand0.toString().padLeft(3)
-            def operatorPadded = operator.toString().padLeft(1)
-            def operator1Padded = operand1.toString().padLeft(3)
-
-            def expression = "${operand0Padded} ${operatorPadded} ${operator1Padded}"
             int result = Eval.me(expression) as int
             if (!results.contains(result)) {
                 results << result
-
-                println expression
             }
         }
 
@@ -32,9 +37,14 @@ class Application {
             oddOneOut = random.nextInt(MAX_VALUE * 3)
         }
 
-        println("---")
-        results.toList().shuffled().forEach { println(it.toString().padLeft(3)) }
-        println("---")
-        println(oddOneOut.toString().padLeft(3))
+        def possibleResults = results.toList().shuffled().collect()
+
+        return new Exercise(expressions: expressions, possibleResults: possibleResults)
+   }
+
+    @ToString
+    private static class Exercise {
+        Set<String> expressions
+        Set<Integer> possibleResults
     }
 }
